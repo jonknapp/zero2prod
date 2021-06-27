0a87f86369b1083891d4313148ad0bb117be1b0f
Jonathan Knapp <jon@coffeeandcode.com>
2021-06-27 12:12:31 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "zero2prod";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit overlays system; };
+      in {
+        devShell = pkgs.mkShell {
+          name = "zero2prod";
+
+          buildInputs = [
+            pkgs.libgit2 # used for cargo-tarpaulin
+            pkgs.nixfmt
+            pkgs.openssl
+            pkgs.pkgconfig
+            (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override { extensions = [ "rust-src" ]; }))
+          ] ++ (if system == "x86_64-darwin" then
+            [ pkgs.darwin.apple_sdk.frameworks.Security ]
+          else
+            [ ]);
+        };
+      });
+}
