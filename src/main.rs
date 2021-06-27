ff12e0d765158b185f338a8870957bd3ff1ebde9
Jonathan Knapp <jon@coffeeandcode.com>
2021-06-27 12:36:30 -0400

    Refactor server address to handle dynamic port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::env;
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let url = format!(
+        "127.0.0.1:{}",
+        env::var("PORT").unwrap_or(String::from("8000"))
+    );
+    let listener = TcpListener::bind(url).expect("Failed to bind to address");
+    let port = listener.local_addr().unwrap().port();
+    println!(
+        "Starting server at {}",
+        format!("http://127.0.0.1:{}", port)
+    );
+    run(listener)?.await
 }
