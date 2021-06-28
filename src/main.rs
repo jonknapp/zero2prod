33966822fa738e296ec2156b3a797ebb04d6d937
Jonathan Knapp <jon@coffeeandcode.com>
2021-06-27 22:07:53 -0400

    Add configuration and connect to postgres in integration test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::env;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let url = format!(
         "127.0.0.1:{}",
-        env::var("PORT").unwrap_or(String::from("8000"))
+        env::var("PORT").unwrap_or(configuration.application_port.to_string())
     );
     let listener = TcpListener::bind(url).expect("Failed to bind to address");
     let port = listener.local_addr().unwrap().port();
