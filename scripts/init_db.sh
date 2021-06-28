4cc3a3e05ffd701e3c43b3a192e2d35620ac94ad
Jonathan Knapp <jon@coffeeandcode.com>
2021-06-27 22:11:28 -0400

    DB init script tweaks

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,14 +7,14 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-if [[-z "${SKIP_DOCKER}"]]
+if [[ -z "${SKIP_DOCKER}" ]]
 then
   container_id=$(docker run \
     -e POSTGRES_USER="${DB_USER}" \
     -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
     -e POSTGRES_DB="${DB_NAME}" \
     -p "${DB_PORT}":5432 \
-    --name zero2prod \
+    --name zero2prod-db
     -d postgres \
     postgres -N 1000)
 
